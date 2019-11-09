50d9ffbddb11046e0f2ea1796769fee958e096df
Gosainmanish <57554106+Gosainmanish@users.noreply.github.com>
2019-11-09 12:14:01 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+fact(int n)
+{
+    if (n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}
+
+main()
+{
+    int m,v;
+    printf("\n enter any number :");
+    scanf("%d",&m);
+        v=fact(m);
+printf("\n FACTORIAL : %d ",v);
+}
